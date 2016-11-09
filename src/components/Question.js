ddb78aecd1353013d32e9f53f5f2631981e41cde
Adam Bernard <ab255@me.com>
2016-11-08 19:23:31 -0700

    add original functionality

---
+++ src/components/Question.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import '../styles/Question.css';
+
+
+export default class Question extends Component {
+  constructor(props) {
+    super()
+  }
+
+  render() {
+    console.log(this.props);
+    return(
+      <div className='question-answer'>
+        <div className='question-title-background'>
+          <h3 className='question-title'>{this.props.title}</h3>
+        </div>
+        {this.props.answers.map((answer ,i) => {
+          return(
+            <div key={i} className='answer'>
+              <label>
+                <input type='checkbox' value={answer.score} className='checkbox'/>
+                  {answer.title}
+              </label>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
